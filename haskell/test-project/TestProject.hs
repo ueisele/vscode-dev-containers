c9a74e3d49b5df8896bd89e164623a43093fceb5
Uwe Eisele <code@uweeisele.eu>
2020-01-10 09:34:17 +0100

    WIP: add phoityne HAskell GHCI Debugger

--- haskell/test-project/TestProject.hs
+++ haskell/test-project/TestProject.hs
@@ -1,2 +1,10 @@
 main :: IO ()
-main = putStrLn "Hello Haskell :)"
+main = putStrLn output
+
+output :: String
+output = "Hello Haskell :) " ++ show (sign 12)
+
+sign :: Int -> Int
+sign n  | n < 0     = -1
+        | n == 0    = 0
+        | otherwise = 1
